bc397c1debbc6410821debfc02413dc6cadcc20d
Jeff Golden <gjgolden08@gmail.com>
2020-11-17 23:16:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,5 @@
 require 'pry' 
 
-def using_concat
-  my_favorite_things = ["raindrops on roses","whiskers on kittens"]
-  more_favs = ["sportscars","flatiron school"]
-  all_my_favs = [my_favorite_things.concat(more_favs)]
-  all_my_favs
+def using_concat(array, array2)
+  array.concat(array2)
 end
